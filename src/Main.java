cbcec82d53bb3b691c6e5ff39b31cf0068433d9f
euphoriaqn <euphoriaqn@gmail.com>
2017-09-13 00:15:11 +0300

    реалізовано двері

--- src/Main.java
+++ src/Main.java
@@ -20,21 +20,31 @@ public class Main {
         final Semaphore SEMAPHORE = new Semaphore(in.nextInt());
 
         for(int i = 0; i < peopleCount; i++) {
-
+            final int x = i+1;
             new Thread(()->{
-                System.out.printf("Поток № [%d] пришел к входу в библиотеку\n", Thread.currentThread().getId());
+                System.out.printf("Поток № [%d] пришел к двери входа в библиотеку c улицы\n", x);
                 try {
-                    if (SEMAPHORE.hasQueuedThreads()) System.out.printf("Поток № [%d] ждет входа в библиотеку\n", Thread.currentThread().getId());
+                    synchronized (LIBRARY) {
+                        System.out.printf("Поток № [%d] проходит через дверь внутрь\n", x);
+                        Thread.sleep(500);
+                    }
+                    if (SEMAPHORE.hasQueuedThreads()) System.out.printf("Поток № [%d] ждет входа в библиотеку\n", x);
                     SEMAPHORE.acquire();
-                    System.out.printf("Поток № [%d] вошел в библиотеку\n", Thread.currentThread().getId());
-                    System.out.printf("Поток № [%d] читает книгу\n", Thread.currentThread().getId());
+                    System.out.printf("Поток № [%d] вошел в библиотеку\n", x);
+                    System.out.printf("Поток № [%d] читает книгу\n", x);
                     int millisecondsToSleep = r.nextInt(4000) + 1000;
                     Thread.sleep(millisecondsToSleep);
+                    System.out.printf("Поток № [%d] подошел к двери изнутри \n", x);
+                    synchronized (LIBRARY) {
+                        System.out.printf("Поток № [%d] проходит через дверь наружу\n", x);
+                        Thread.sleep(500);
+                    }
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+
                 SEMAPHORE.release();
-                System.err.printf("Поток № [%d] вішел с библиотеки\n", Thread.currentThread().getId());
+                System.err.printf("Поток № [%d] вышел с библиотеки\n", x);
             }).start();
 //            thread.setPriority(Thread.MAX_PRIORITY);
 //            thread.start();
